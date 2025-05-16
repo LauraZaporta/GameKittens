2c052617fce7a96b89774bb453ec4d572674d093
LauraZaporta <laura.zaporta.7e8@itb.cat>
2025-05-16 10:32:40 +0200

    Initial setup. Some directories need to change.

---
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/App.android.kt
@@ -0,0 +1,21 @@
+package cat.itb.m78.exercices
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import cat.itb.m78.exercices.mapsApp.Navigation
+
+class AppActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent { Navigation() }
+    }
+}
+
+@Preview
+@Composable
+fun AppPreview() { Navigation() }
