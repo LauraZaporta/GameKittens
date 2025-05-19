be2f1274c6d533f25e6922b6b3a0b6eba006f8a9
LauraZaporta <laura.zaporta.7e8@itb.cat>
2025-05-19 10:21:55 +0200

    Updated class Task (added id)

--- composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/EcoPetsProject/DTOs/Task.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/EcoPetsProject/DTOs/Task.kt
@@ -1,7 +1,8 @@
 package cat.itb.m78.exercices.EcoPetsProject.DTOs
 
 class Task(
-    val titel: String,
+    val id: Int,
+    val title: String,
     val description: String,
     val imageURI: String,
     val userName: String //Seleccionar el nom d'usuari de l'empleat a partir del seu ID a la BD
