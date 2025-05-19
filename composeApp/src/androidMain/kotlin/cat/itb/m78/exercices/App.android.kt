d219f22dba984729aa00ab57aca575a460bf5ad5
LauraZaporta <laura.zaporta.7e8@itb.cat>
2025-05-19 09:52:18 +0200

    Navigation initialized

--- composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/App.android.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/App.android.kt
@@ -1,5 +1,6 @@
 package cat.itb.m78.exercices
 
+import Navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,10 +12,10 @@ class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { }
+        setContent { Navigation() }
     }
 }
 
 @Preview
 @Composable
-fun AppPreview() { }
+fun AppPreview() { Navigation() }
