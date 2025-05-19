e7018f6a3cab03c987f873056cb2c59240953c40
Unai <unai.arevalo.7e8@itb.cat>
2025-05-19 09:58:03 +0200

    Tasks Screen first push

--- composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/EcoPetsProject/Screens/ScreenDetailsTask.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/EcoPetsProject/Screens/ScreenDetailsTask.kt
@@ -1,2 +1,19 @@
 package cat.itb.m78.exercices.EcoPetsProject.Screens
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+
+
+@Composable
+fun TaskInfo(
+    task: Task
+){
+    AsyncImage(
+        model = task.imageURI,
+        contentDescription = "sustainable image",
+        modifier = Modifier.size(400.dp, 200.dp)
+    )
+}
