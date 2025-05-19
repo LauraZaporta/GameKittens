aa0f6bbec56676201b48a53bf1e37569d6911171
Unai <unai.arevalo.7e8@itb.cat>
2025-05-19 11:31:23 +0200

    Tasks Screen 19/05/2025 11:31 update

--- composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/EcoPetsProject/Screens/ScreenListTasks.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/EcoPetsProject/Screens/ScreenListTasks.kt
@@ -3,12 +3,16 @@ package cat.itb.m78.exercices.EcoPetsProject.Screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -20,20 +24,31 @@ import cat.itb.m78.exercices.EcoPetsProject.DTOs.Task
 @Composable
 fun ScreenListTasks(
     tasks: List<Task>,
-    navigateToAddTaskScreen:() -> Unit,
-    navigateToTaskInfo: (Int) -> Unit
+    filterString: String,
+    onUpdateFilter: (String) -> Unit,
+    atributToSearch: String,
+    onUpdateAtributFilter:(String) -> Unit,
+    navigateToScreenAddTask:() -> Unit,
+    navigateToScreenDetailsTask: (Int) -> Unit
 ){
     Column (horizontalAlignment = Alignment.CenterHorizontally){
-        Button(
-            onClick = { navigateToAddTaskScreen() }
-        ) {
-            Text("Send Sustainable Task", textAlign = TextAlign.Center)
+        Row {
+            Button(
+                onClick = { navigateToScreenAddTask() }
+            ) {
+                Text("Send Sustainable Task", textAlign = TextAlign.Center)
+            }
+            TextField(
+                value = filterString,
+                label = { Text(text = "") },
+                onValueChange = { onUpdateFilter(it) }
+            )
         }
         Text("Tasks List:", fontSize = 30.sp, fontWeight = FontWeight(800))
         LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
             for (task in tasks) {
                 item {
-                    Box (modifier = Modifier.clickable { navigateToTaskInfo(task.id) }) {
+                    Box (modifier = Modifier.clickable { navigateToScreenDetailsTask(task.id) }) {
                         Text(" - ${task.title}", fontSize = 20.sp)
                         Text("   made by: ${task.employee.name}")
                         Spacer(modifier = Modifier.size(30.dp))
--- composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/EcoPetsProject/ViewModels/VMListTasks.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/m78/exercices/EcoPetsProject/ViewModels/VMListTasks.kt
@@ -1,9 +1,16 @@
 package cat.itb.m78.exercices.EcoPetsProject.ViewModels
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import cat.itb.m78.exercices.EcoPetsProject.DTOs.Task
 
 class TasksListViewModel : ViewModel(){
     val tasksList: MutableList<Task>? = null
+    val filterString = mutableStateOf("")
+    val atributToSearch = mutableStateOf( "userName" )
 
+    fun onUpdateFilter(newFilterString: String){
+        filterString.value = newFilterString
+    }
 }
