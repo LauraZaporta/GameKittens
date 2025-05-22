82aa35b6d1693ffa715fb94838714fff6291b09d
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-05-22 10:45:20 +0200

    Auth and Roles setup finished. Closes #14

--- API.GameKittens/API.GameKittens/Program.cs
+++ API.GameKittens/API.GameKittens/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Text;
 using System.Text.Json.Serialization;
 using API.GameKittens.Context;
 using API.GameKittens.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 public class Program
 {
@@ -51,15 +55,71 @@ public class Program
                  .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
 
+        //Configuracio del Token i les seves validacions
+        var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings["Audience"],
+
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
+                };
+            });
+
+        builder.Services.AddAuthorization();
+
 
         builder.Services.AddRazorPages();
         builder.Services.AddHttpContextAccessor();
 
         // Swagger
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(opt =>
+        {
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer"
+            });
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+        });
 
-        // Controladors
-        builder.Services.AddControllers();
+        // Controladors and ignore cycles
+        builder.Services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
 
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
@@ -70,6 +130,12 @@ public class Program
         var app = builder.Build();
         /*--------------*/
 
+        // Add initial roles: User, Boss and Admin
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            await API.GameKittens.Tools.RoleTools.CrearRolsInicials(services);
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
@@ -81,6 +147,7 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapControllers();
---
+++ API.GameKittens/API.GameKittens/Tools/RoleTools.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API.GameKittens.Tools
+{
+    public class RoleTools
+    {
+        public static async Task CrearRolsInicials(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] rols = { "Admin", "Boss", "User" };
+
+            foreach (var rol in rols)
+            {
+                if (!await roleManager.RoleExistsAsync(rol))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(rol));
+                }
+            }
+        }
+    }
+}
